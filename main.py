b7c62263bec1aa72f3381511c4f4f0c6c534082d
Jason <jasonchong0226@gmail.com>
2019-05-02 17:46:11 -0700

    Changing connecting-flight to connecting_flight, and adding main file.

---
+++ main.py
@@ -0,0 +1,11 @@
+from database import Database
+from connecting_flights import ConnectingFlight
+
+
+if __name__ == '__main__':
+    db = Database('localhost', 27017, "connecting_flight")
+    cf = ConnectingFlight(db)
+    cf.add_many_flight([["KLAX", "KSFO", {"price":203.34, "time":124}]])
+    # result = db.all_flights_from("KLAX")
+    # for flight in result:
+    #     print(flight)
