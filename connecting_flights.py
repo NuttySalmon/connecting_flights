9d9f75149635502f761a05815f7d0b57fd28c45c
NuttySalmon <nuttysalmon@gmail.com>
2019-05-05 16:58:50 -0700

    Implemented weight comparison in pq

--- connecting_flights.py
+++ connecting_flights.py
@@ -1,44 +1,57 @@
 from database import Database
-import heapq
+from collections import OrderedDict
+
 
 class ConnectingFlight:
     def __init__(self, database):
         self.db = database
-        
+
     def add_one_flight(self, orig, dest, **kwargs):
-        db.add_flight(orig, dest, kwargs)
+        self.db.add_flight(orig, dest, kwargs)
         self.calc_all()
 
     def add_many_flight(self, arr):
 
         for new_flight in arr:
-            db.add_flight(new_flight[0], new_flight[1], **new_flight[2])
+            self.db.add_flight(new_flight[0], new_flight[1], **new_flight[2])
 
         self.calc_all()
 
     def calc_all(self):
         for cri in Database.Criterion:
-            print(cri)
+            print("Criteron: {}".format(cri.name))
             for airport in self.db.all_airports():
-                print(airport)
+                print("{}:".format(airport["icao"]))
                 self.dijkstra(cri, airport)
-
 
     def dijkstra(self, criterion, airport):
         shortest_path = []
         weight_name = criterion.name
-        pq  = []
+        currWeight = 0
+        pq = OrderedDict()
         orig = airport["icao"]
+        self.addConnected(pq, orig, currWeight, weight_name)
+        pq = OrderedDict(sorted(pq.items(), key=lambda x: x[1][0]))
+        print(pq.keys())
+        print(pq.items())
+
+    def addConnected(self, pq, orig, currWeight, weight_name):
         connected = self.db.all_flights_from(orig)
         for flight in connected:
-            heapq.heappush(pq, (flight[weight_name], [flight["orig"], flight["dest"]]))
-        print(pq)
+            f_dest = flight["dest"]
+            f_w = flight[weight_name]
+            try:
+                oldWeight = pq[f_dest][0]
+                newWeight = f_w + currWeight
+                if newWeight < oldWeight:
+                    pq[f_dest] = (newWeight, flight)
+
+            except KeyError:
+                pq[f_dest] = (f_w, flight)
+
+       
 
 
-if __name__ == '__main__':
-    db = Database('localhost', 27017, "connecting_flight")
-    cf = ConnectingFlight(db)
-    cf.add_many_flight([["KLAX", "KSFO", {"price":203.34, "time":124}]])
     # result = db.all_flights_from("KLAX")
     # for flight in result:
     #     print(flight)
