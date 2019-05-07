db614be5cc42c6a635009e2e56c00b47d2e4fb35
NuttySalmon <nuttysalmon@gmail.com>
2019-05-06 21:15:47 -0700

    Removed Dijkstra

--- connecting_flights.py
+++ connecting_flights.py
@@ -31,38 +31,6 @@ class ConnectingFlight:
                 # print("{}:".format(airport["id"]))
                 self.dijkstra(cri, airport)
             '''
-    def dijkstra(self, criterion, airport):
-        shortest_path = []
-        weight_name = criterion.name
-        currWeight = 0
-        pq = OrderedDict()
-        orig = airport["id"]
-        pq = self.add_connected(pq, orig, currWeight, weight_name)
-        curr = None
-        while(len(pq) != 0):
-            curr = pq.popitem(last=False)
-            shortest_path.append(curr)
-            print("procressing {}".format(curr[0]))
-            pq = self.add_connected(pq=pq, orig=curr[0], currWeight=curr[1][0],
-                                    weight_name=weight_name)
-        for p in shortest_path:
-            print("{}->{}".format(p[1][1]["orig"], p[1][1]["dest"]))
-
-    def add_connected(self, pq, orig, currWeight, weight_name):
-        connected = self.db.all_flights_from(orig)
-        for flight in connected:
-            f_dest = flight["dest"]
-            f_w = flight[weight_name]
-            try:
-                oldWeight = pq[f_dest][0]
-                newWeight = f_w + currWeight
-                if newWeight < oldWeight:
-                    pq[f_dest] = (newWeight, flight)
-
-            except KeyError:
-                pq[f_dest] = (f_w, flight)
-
-        return OrderedDict(sorted(pq.items(), key=lambda x: x[1][0]))
 
     def floyd_warshal(self, criterion):
         '''
