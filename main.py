1f951910c3caa70511de8a720c1f056862c77863
Tony <tsaechao19@gmail.com>
2019-05-08 21:32:03 -0700

    fixed add origin and destination airport and added in user entry for time and price

--- main.py
+++ main.py
@@ -18,24 +18,43 @@ if __name__ == '__main__':
                         ["d", "f", {"price": 11, "time": 3}]])
                         """
 
+
     # cf.print_floyd_warshal(Database.Criterion.price)
-    print("Welcome, Choose an option: \n 1. Add Airport \n 2. Find shortest path")
-    price = db.Criterion.price
-    time = db.Criterion.time
-    distance = db.Criterion.distance
-    option = input()
-    if option == "1":
-        origin_add = input("Enter an origin airport: ")
-        dest_add = input("Enter a destination airport: ")
-        db.add_flight(origin_add, dest_add)
-    if option == "2":
-        origin_shortest = input("Enter an origin airport: ")
-        dest_shortest = input("Enter a destination airport: ")
-        crit = input("Choose criterion: \n 1. Time \n 2. Price \n 3. Distance ")
-        if crit == "1":
-            crit = time
-        elif crit == "2":
-            crit = price
-        elif crit == "3":
-            crit = distance
-        cf.get_shortest_floyd_warshal(crit, origin_shortest, dest_shortest)
+    def menu():
+        print(
+            "Welcome, choose an option (Enter 'q' to quit) : \n 1. Add Airport \n 2. Add  flight \n 3. Find shortest path")
+        price = db.Criterion.price
+        time = db.Criterion.time
+        distance = db.Criterion.distance
+        option = input()
+        loop = True
+        while loop is True:
+            if option == "1":
+                airport_add = input("Enter an airport: ")
+                db.add_airport(airport_add)
+                menu()
+            elif option == "2":
+                origin_add = input("Enter an origin airport: ")
+                dest_add = input("Enter a destination airport: ")
+                price = input("Enter price: ")
+                time = input("Enter time: ")
+                cf.add_many_flight([[origin_add, dest_add, {"price": price, "time": time}]])
+                menu()
+            elif option == "3":
+                origin_shortest = input("Enter an origin airport: ")
+                dest_shortest = input("Enter a destination airport: ")
+                print("Choose criterion: \n 1. Time \n 2. Price \n 3. Distance ")
+                crit = input()
+                if crit == "1":
+                    crit = time
+                elif crit == "2":
+                    crit = price
+                elif crit == "3":
+                    crit = distance
+                cf.get_shortest_floyd_warshal(crit, origin_shortest, dest_shortest)
+                cf.print_floyd_warshal(crit)
+            elif option == "q":
+                break
+            else:
+                break
+    menu()
