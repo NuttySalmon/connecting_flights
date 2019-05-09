9e8cfa0520fcec3d71a09e74589b9d967feda6a6
NuttySalmon <nuttysalmon@gmail.com>
2019-05-09 02:19:08 -0700

    Added import call. Added flight details. Cleaned up.

--- main.py
+++ main.py
@@ -32,21 +32,25 @@ if __name__ == '__main__':
     db = Database('localhost', 27017, "connecting_flight")
     db.drop_database()
     cf = ConnectingFlight(db)
+    import_route_csv(cf, "1")
     # data from: https://en.wikipedia.org/wiki/Shortest_path_problem#/media/File:Shortest_path_with_direct_weights.svg
-    """cf.add_many_flight([["a", "b", {"price": 4, "time": 1}],
+    '''
+    cf.add_one_flight("a", "b", price=4, time=1)
+    cf.add_many_flight([["a", "b", {"price": 4, "time": 1}],
                         ["a", "c", {"price": 2, "time": 2}],
                         ["b", "c", {"price": 5, "time": 3}],
                         ["b", "d", {"price": 10, "time": 3}],
                         ["c", "e", {"price": 3, "time": 3}],
                         ["e", "d", {"price": 4, "time": 3}],
                         ["d", "f", {"price": 11, "time": 3}]])
-                        """
+    
     # cf.print_floyd_warshal(Database.Criterion.price)
+    '''
 
     def menu():
         print("Welcome, choose an option (Enter 'q' to quit) : \n 1. Add Airport \n 2. Add  flight \n 3. Find shortest path")
         price = db.Criterion.price
-        time = db.Criterion.time
+        duration = db.Criterion.duration
         distance = db.Criterion.distance
         option = input()
         loop = True
@@ -57,26 +61,36 @@ if __name__ == '__main__':
                 loop = False
                 menu()
             elif option == "2":
+
+                airline = input("Enter airline (IATA): ")
+                no = input("Enter flight number: ")
                 origin_add = input("Enter an origin airport: ")
                 dest_add = input("Enter a destination airport: ")
                 price = input("Enter price: ")
-                time = input("Enter time: ")
+                duration = input("Enter duration: ")
                 distance = input("Enter distance: ")
-                cf.add_many_flight([[origin_add, dest_add, {"price": price, "time": time, "distance": distance}]])
+                cf.add_one_flight(origin_add, dest_add,
+                                  **{"price": price,
+                                   "duration": duration,
+                                   "distance": distance,
+                                   "airline": airline,
+                                   "no": no})
                 loop = False
                 menu()
             elif option == "3":
                 origin_shortest = input("Enter an origin airport: ")
                 dest_shortest = input("Enter a destination airport: ")
-                print("Choose criterion: \n 1. Time \n 2. Price \n 3. Distance ")
+                print("Choose criterion:")
+                for c in Database.Criterion:
+                    print("{}. {}".format(c.value, c.name))
                 crit = input()
-                if crit == "1":
-                    crit = time
-                elif crit == "2":
-                    crit = price
-                elif crit == "3":
-                    crit = distance
-                short = cf.get_shortest_floyd_warshal(crit, origin_shortest, dest_shortest)
+
+                try:
+                    cf.get_shortest_floyd_warshal(crit, origin_shortest, dest_shortest)
+                except ValueError:
+                    print("Invalid choice")
+                    break
+
                 print(short)
                 loop = False
                 menu()
