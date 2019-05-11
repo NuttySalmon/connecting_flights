bf4c8e60cfdf90210e8007004f7722c856e40bbf
NuttySalmon <nuttysalmon@gmail.com>
2019-05-11 03:05:32 -0700

    Added comments to ConnectingFlights

--- shortest/ConnectingFlights.py
+++ shortest/ConnectingFlights.py
@@ -2,26 +2,28 @@ from . import Database
 import time
 
 class ConnectingFlights:
+    """Class to calculate ConnectingFlights"""
     def __init__(self, database):
+        """Constructor. Takes Database object"""
         self.db = database
 
     def add_one_flight(self, orig, dest, **kwargs):
+        """Add one flight to database. Accept multiple weights as kwrags"""
         print(kwargs)
         self.db.add_flight(orig, dest, **kwargs)
-        self.calc_all()
+        self.calc_all() # calculate shortest path
 
     def add_many_flights(self, arr):
-        '''
-        Accept list of list, format: [orig, dest, **weights]
-        '''
+        """Accept list of list, format: [orig, dest, **weight]"""
         for new_flight in arr:
             orig = new_flight[0]
             dest = new_flight[1]
             self.db.add_flight(orig, dest, **new_flight[2])
 
-        self.calc_all()
+        self.calc_all()  # calculate shortest path
 
     def calc_all(self):
+        """Calculate adjency table for all critera"""
         self.db.clear_adj()
         print("Doing calculation...")
         # calculate shortest path for each criterion
@@ -30,19 +32,17 @@ class ConnectingFlights:
             t1 = time.time()
             self.floyd_warshal(cri)
             t2 = time.time()
-            print("used {}s.\n".format(t2-t1))
-            
+            print("used {}s.\n".format(t2-t1))  # print time took for calculation
+
     def floyd_warshal(self, criterion):
-        '''
-        Generate shortest path relatinship in db using floyd warshal algorithm
-        '''
+        """Generate shortest path data in db using floyd warshal algorithm"""
 
         all_airports = self.db.all_airports_list()
 
-        # clear adjacency list
+        # init adjacency list
         self.make_adj(all_airports, criterion)
 
-        for thru in all_airports:   # intermediate vertex to consider
+        for thru in all_airports:   # thru = intermediate vertex to consider
             for orig in all_airports:
                 if orig == thru:
                     continue  # continue if intermediate is same as origin
@@ -59,27 +59,28 @@ class ConnectingFlights:
                         # do nothing when not accessable thrugh intermediate
                         continue 
 
-                    # calculate new weight if using intermediate vertex
+                    # calculate new weight when going through intermediate "thru" vertex
                     new_weight = float(orig_to_thru["weight"]) + float(thru_to_dest["weight"])
+
                     last_path = thru_to_dest["thru"]
                     last_flight = thru_to_dest["last_flight"]
-                    orig_adj = self.db.get_adj(criterion, orig, dest)
 
-                    if orig_adj is None:  # if no path was discovered before
+                    # get existing adjency entry
+                    orig_adj = self.db.get_adj(criterion, orig, dest) 
+
+                    if orig_adj is None:  # if no path was discovered before, add.
                         self.db.add_to_adj(criterion, orig, dest, last_path, 
                                            new_weight, last_flight)
                     else:
+                        # if new weight total is shorter,
+                        # use intermediate vertex as part of shortest path
                         old_weight = float(orig_adj["weight"])
                         if new_weight < old_weight:
                             self.db.update_adj(criterion, orig, dest,
                                                last_path, new_weight, last_flight)
 
-        
-
     def print_floyd_warshal(self, criterion):
-        '''
-        Print shortest path for each origin and destination pair
-        '''
+        """Print shortest path for each origin and destination pair"""
 
         # get string list of all airports
         all_airports = self.db.all_airports_list()
@@ -107,36 +108,34 @@ class ConnectingFlights:
                     print("No data")
 
     def get_shortest_floyd_warshal(self, criterion, orig, dest):
-        '''
-        Take criterion, origin, and destination and return shortest path info
-        '''
-
+        """Take criterion, origin, and destination and return shortest path info"""
         output = {}
         shortest = []
 
         start = orig
         end = dest
         total = 0.0
-        while start != end:
+        while start != end:  # backtrack to find shortest path
+            # get shortest from start to end
             adj = self.db.get_adj(criterion, start, end)
-            if adj is None:
+
+            if adj is None:  # break if no data found
                 break
-            mid = adj["thru"]
+
+            mid = adj["thru"]  # get the last vertex before the end in shortest path
             weight = float(self.db.get_weight(criterion, mid, end))
             airline_id = adj["last_flight"]
             shortest.append((mid, end, weight, airline_id))
             total += weight
-            end = mid
+            end = mid   # backtracking by using intermediate point as end point
 
-        output["path"] = shortest[::-1]
+        output["path"] = shortest[::-1]  # reverse list
         output["total_weight"] = total
         return output
 
     @staticmethod
     def get_str_from_cri(criterion, target):
-        '''
-        Return formatted string with associated unit
-        '''
+        """Return formatted string with associated unit"""
 
         if criterion == Database.Criterion.price:
             return "${}".format('%.2f' % target)
@@ -146,18 +145,16 @@ class ConnectingFlights:
             return "{} miles".format(target)
 
     def make_adj(self, all_airports, criterion):
-        '''
-        Initialize adj with directly connected airports
-        '''
+        """Initialize adj with flights"""
 
         weight_name = criterion.name
-        for orig in all_airports:
+        for orig in all_airports:  # calculate for all airport
             connected = self.db.all_flights_from(orig)
             for flight in connected:
                 dest = flight["dest"]
                 try:
                     last_flight = (flight["airline"], flight["no"])
-                except KeyError:
+                except KeyError:  # if not found
                     last_flight = None
                 try:
                     weight = float(flight[weight_name])
